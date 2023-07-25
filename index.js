44ec805c8aa067ab2b95762f635753e366036c0d
PK Warlock <pyush0908@gmail.com>
2023-07-26 00:23:01 +0530

    final

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ let el_box=document.querySelector(".allbox");
 let el_res=document.querySelector(".result");
 let each_box=document.getElementsByClassName("box")
 let in_box=document.getElementsByClassName("inbox")
+let new_inbox=document.querySelectorAll(".inbox")
 let btn_res=document.querySelector(".restart")
+// let winLine=document.querySelector(".line");
 let turn ="X";
+let draw=[]
 let gameOver=false;
+
 
 const change=()=>
 {
@@ -20,33 +24,36 @@ Array.from(each_box).forEach((ele)=>
     ele.addEventListener("click",()=>
     {
         let in_box=ele.querySelector(".inbox")
-        if(in_box.innerHTML==="")
+        if(in_box.innerHTML==="" && draw.length<9)
         {
-        in_box.innerHTML=turn
-        turn=change()
+            draw.push(turn);
+        in_box.innerHTML=turn;
+        turn=change();
         won();
         if(!gameOver)
         {
             el_res.innerHTML=turn+"'s TURN";
         }
     }
+    if(draw.length==9 && !gameOver)
+    {
+         el_res.innerHTML="No Result!"
+    }
     })
 })
-
-
 
 const won=()=>
 {
    
     let wonar=[
-               [0,1,2],
-               [3,4,5],
-               [6,7,8],
-               [0,3,6],
-               [1,4,7],
-               [2,5,8],
-               [0,4,8],
-               [2,4,6]
+               [0,1,2,10,45,0],
+               [3,4,5,20,45,0],
+               [6,7,8,30,45,0],
+               [0,3,6,20,38,90],
+               [1,4,7,20,45,90],
+               [2,5,8,20,54,90],
+               [0,4,8,20,45,45],
+               [2,4,6,20,45,135]
             ]
     wonar.forEach(e=>
         {
@@ -55,6 +62,12 @@ const won=()=>
             {
                 el_res.innerHTML= in_box[e[0]].innerHTML+" won The game";
                 gameOver=true;
+                in_box[e[0]].classList.add("winner");
+                in_box[e[1]].classList.add("winner");
+                in_box[e[2]].classList.add("winner");
+                // winLine.style.transform=`translate(${e[4]}vw, ${e[3]}vw) rotate(${e[5]}deg)`;
+                //winLine.style.width="34vw";
+                el_box.classList.add("disabled");
             }
             
         });
@@ -62,12 +75,19 @@ const won=()=>
 
 btn_res.addEventListener("click",()=>
 {
-    
     Array.from(in_box).forEach(ele=>
         {
             ele.innerHTML=""
+            if(ele.classList.contains("winner"))
+            {
+                ele.classList.remove("winner");
+            }
+            el_box.classList.remove("disabled")
+            //  winLine.style.width = "0vw";
+             draw=[]
         })
+        turn="X"
          el_res.innerHTML="X's TURN";
-        gameOver=false;
+        gameOver=false;  
 })
 
